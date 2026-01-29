0fdf22bc11eec2cc38f23ff4a7db81474b3b67b5
valay30 <patelvalay308@gmail.com>
2026-01-29 13:55:37 +0530

    PHP-004 - loop : for loop to print a multiplication table for 5

---
+++ PHP-Practice-Plan/Practice 2 - Control Structures & Loops/PHP-004.php
@@ -0,0 +1,9 @@
+<?php
+
+// Use for loop to print a multiplication table for 5.
+echo "<h3>Multiplication Table for 5:</h3>";
+for ($i = 1; $i <= 10; $i++) {
+    echo "5 x $i = " . (5 * $i) . "<br>";
+}
+
+?>
