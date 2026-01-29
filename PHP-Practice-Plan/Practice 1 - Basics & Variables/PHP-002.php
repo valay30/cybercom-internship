b6abcce38cccaad860965c64716bc74c10ad65f8
valay30 <patelvalay308@gmail.com>
2026-01-29 13:30:49 +0530

    PHP-002 - Array : print_r to show raw structure

--- PHP-Practice-Plan/Practice 1 - Basics & Variables/PHP-002.php
+++ PHP-Practice-Plan/Practice 1 - Basics & Variables/PHP-002.php
@@ -7,4 +7,10 @@ $person = [
     "Skills" => ["PHP", "HTML", "CSS","JS","CLOUD"]
 ];
 
+// print_r to show the raw structure.
+echo "<h3>print_r output:</h3>";
+echo "<pre>";
+print_r($person);
+echo "</pre>";
+
 ?>
