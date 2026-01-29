8bd8b0f345cd5058b1dc66a4dbba1cc8f4dab3da
valay30 <patelvalay308@gmail.com>
2026-01-29 14:01:58 +0530

    PHP-004 - loop : while loop to print a multiplication table for 5

--- PHP-Practice-Plan/Practice 2 - Control Structures & Loops/PHP-004.php
+++ PHP-Practice-Plan/Practice 2 - Control Structures & Loops/PHP-004.php
@@ -1,9 +1,17 @@
 <?php
 
 // Use for loop to print a multiplication table for 5.
-echo "<h3>Multiplication Table for 5:</h3>";
+echo "<h3>Multiplication Table for 5 (for loop):</h3>";
 for ($i = 1; $i <= 10; $i++) {
     echo "5 x $i = " . (5 * $i) . "<br>";
+}
+
+// Multiplication Table of 5 using while loop
+echo "<h3>Multiplication Table for 5 (while loop):</h3>";
+$i = 1;
+while ($i <= 10) {
+    echo "5 x $i = " . (5 * $i) . "<br>";
+    $i++;
 }
 
 ?>
