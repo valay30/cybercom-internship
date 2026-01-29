e89760faf3808ff3343c8e2cc16d020ed0630788
valay30 <patelvalay308@gmail.com>
2026-01-29 13:23:16 +0530

    PHP-001 : Variables: Defined for other datatypes & print.

--- PHP-Practice-Plan/Practice 1 - Basics & Variables/PHP-001.php
+++ PHP-Practice-Plan/Practice 1 - Basics & Variables/PHP-001.php
@@ -4,6 +4,15 @@ $greeting = "Hello";       // String
 $year = 2026;              // Integer
 $isActive = true;          // Boolean
 $bool2 = false;            // Boolean
+$price = 99.99;            // Float
+$items = ["Book", "Pen"];  // Array
+$noValue = null;           // Null
+
+class Student {            // Object
+    public $name = "Valay";
+}
+
+$s = new Student();
 
 echo $greeting . ", the year is " . $year ;
 echo "<br>";
@@ -13,5 +22,18 @@ echo "<br>";
 
 echo "bool2: ". $bool2 ;
 echo "<br>";
+
+echo "Price: $" . $price ;
+echo "<br>";
+
+echo "Null Value: " . $noValue ;
+echo "<br>";
+
+echo "items: ". $items[0] ;
+echo "<br>";
+echo "items: ". $items[1] ;
+echo "<br>";
+
+echo $s->name;
 
 ?>
