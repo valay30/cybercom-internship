695955e17c41683c250301525b21aebf59185359
valay30 <patelvalay308@gmail.com>
2026-01-29 13:28:14 +0530

    PHP-002 - Array : Associative array describing a person

---
+++ PHP-Practice-Plan/Practice 1 - Basics & Variables/PHP-002.php
@@ -0,0 +1,10 @@
+<?php
+
+// Associative array describing a person (Name, Age, Skills).
+$person = [
+    "Name" => "Valay",
+    "Age" => 23,
+    "Skills" => ["PHP", "HTML", "CSS","JS","CLOUD"]
+];
+
+?>
