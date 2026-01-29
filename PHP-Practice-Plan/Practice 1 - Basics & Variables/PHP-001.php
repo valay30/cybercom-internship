9263785aebe16e267e7b515b2f7019d34cfac10e
valay30 <patelvalay308@gmail.com>
2026-01-29 13:14:39 +0530

    PHP-001 : Variables: Defined boolian variables and print

--- PHP-Practice-Plan/Practice 1 - Basics & Variables/PHP-001.php
+++ PHP-Practice-Plan/Practice 1 - Basics & Variables/PHP-001.php
@@ -2,7 +2,16 @@
 
 $greeting = "Hello";       // String
 $year = 2026;              // Integer
+$isActive = true;          // Boolean
+$bool2 = false;            // Boolean
 
 echo $greeting . ", the year is " . $year ;
+echo "<br>";
+
+echo "Is Active: " . $isActive ;
+echo "<br>";
+
+echo "bool2: ". $bool2 ;
+echo "<br>";
 
 ?>
