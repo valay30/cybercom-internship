79629fb1a4a0798b14dbcb6a19b591cecee484f4
valay30 <patelvalay308@gmail.com>
2026-01-29 13:48:38 +0530

    PHP-003 - Conditional Statement : Create variable $day & switch to print Weekend or Weekday

--- PHP-Practice-Plan/Practice 2 - Control Structures & Loops/PHP-003.php
+++ PHP-Practice-Plan/Practice 2 - Control Structures & Loops/PHP-003.php
@@ -18,4 +18,43 @@ echo "Marks: $marks, Grade: $grade ";
 echo "<br>";
 
 
+// Create a variable $day. Use switch to print 'Weekend' for Sat/Sun and 'Weekday' for Mon-Fri.
+$day = "Monday";
+
+echo $day . " - ";
+
+switch ($day) {
+
+    case "Saturday":
+        echo "Weekend";
+        break;
+
+    case "Sunday":
+        echo "Weekend";
+        break;
+
+    case "Monday":
+        echo "Weekday";
+        break;
+
+    case "Tuesday":
+        echo "Weekday";
+        break;
+
+    case "Wednesday":
+        echo "Weekday";
+        break;
+
+    case "Thursday":
+        echo "Weekday";
+        break;
+
+    case "Friday":
+        echo "Weekday";
+        break;
+
+    default:
+        echo "Invalid Day";
+}
+
 ?>
